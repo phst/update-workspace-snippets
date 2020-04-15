612102d9f662c22ca3db581654f6ba9d03a91941
Philipp Stephani <phst@google.com>
2020-04-15 20:56:42 +0200

    give updater package some documentation

--- updater/updater.go
+++ updater/updater.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package updater contains the type Updater to update Bazel workspace
+// snippets.
 package updater
 
 import (
